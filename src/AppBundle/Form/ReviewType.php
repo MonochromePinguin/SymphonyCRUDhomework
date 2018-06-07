d8f196d049072aba674bd607ac9aada1a3e963e5
monochromePinguin <jaimaille.com@gmail.com>
2018-06-07 23:43:45 +0200

    Customization of the form from ReviewType

--- src/AppBundle/Form/ReviewType.php
+++ src/AppBundle/Form/ReviewType.php
@@ -2,7 +2,13 @@
 
 namespace AppBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +19,59 @@ class ReviewType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('userRated')->add('reviewAuthor')->add('note')->add('publicationDate')->add('note')->add('text');
+        $builder->add(
+            'userRated',
+            EntityType::class,
+            [
+                'label' => 'Utilisateur noté : ',
+                'class' => 'AppBundle\Entity\User',
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder( 'user')->orderBy('user.lastName', 'ASC');
+                },
+                'choice_label' => 'lastName'
+            ]
+        )
+        ->add(
+        'reviewAuthor',
+            EntityType::class,
+            [
+                'label' => 'Auteur : ',
+                'class' => 'AppBundle\Entity\User',
+                'query_builder' => function(EntityRepository $er) {
+                    return $er->createQueryBuilder( 'user')->orderBy('user.lastName', 'ASC');
+                },
+                'choice_label' => 'lastName'
+            ]
+        )
+        ->add(
+            'note',
+            IntegerType::class,
+            [
+                'label' => 'Note (0 à 5) : ',
+                'attr' => [
+                    'min' => 0,
+                    'max' => 5
+                ]
+            ]
+        )
+        ->add(
+            'publicationDate',
+            DateTimeType::class,
+            [
+                'label' => 'Date de publication : ',
+                'data' => new \DateTime('now')
+            ]
+        )
+        ->add(
+            'text',
+            TextareaType::class,
+            [
+                'label' => 'Avis : ',
+                'attr' => [
+                    'maxlength' => 255
+                ]
+            ]
+        );
     }
     /**
      * {@inheritdoc}
